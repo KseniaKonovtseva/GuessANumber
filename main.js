d248e69fbd9e42491da4d95dd00a305881821e01
Ksenia Konovtseva <ksenia1229@mail.ru>
2023-10-06 00:53:50 +0300

    added counter

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const guessANumber = function() {
+const guessANumber = function(attempt) {
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -11,23 +11,43 @@ const guessANumber = function() {
 
   const game = function() {
     let answer = prompt( "Угадай число от 1 до 100");
+    console.log(attempt);
 
-    if (answer === null) {
-       alert("Игра окончена");
-    } else if (isNaN(answer) || !isFinite(answer) || answer === "") {
-      alert("Введи число!");
-      game();
-    } else if (+answer > randomInt) {
-      alert( "Загаданное число меньше");
-      game();
-    } else if (+answer < randomInt) {
-      alert("Загаданное число больше");
-      game();
-    } else if (+answer === randomInt) {
-      return alert("Поздравляю, Вы угадали!!!");
-    } 
+    if (attempt > 0) {
+
+      if (answer === null) {
+        alert("Игра окончена");
+      } else if (isNaN(answer) || !isFinite(answer) || answer === "") {
+        alert("Введи число!");
+        game();
+      } else if (+answer > randomInt) {
+        alert( "Загаданное число меньше, " + "осталось попыток: " + attempt);
+        attempt--;
+        game();
+      } else if (+answer < randomInt) {
+        alert("Загаданное число больше, " + "осталось попыток: " + attempt);
+        attempt--;
+        game();
+      } else if (+answer === randomInt) {
+        let final = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+
+        if (final === true) {
+          guessANumber(9);
+        } else {
+          return;
+        }
+      } 
+
+    } else {
+      let final = confirm("Попытки закончились, хотите сыграть еще?");
+      if (final === true) {
+        guessANumber(9);
+      } else {
+        return;
+      }
+    }
   }
   game();
 }
 
-guessANumber();
+guessANumber(9);
